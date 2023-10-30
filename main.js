0515830e8953d022be3ed378c0151bf75ee958d0
Raspudic97 <raspudic88@gmail.com>
2023-10-30 22:26:46 +0100

    trigger bonus_hit sound on bonus symbol animation

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 import * as PIXI from "pixi.js";
 import { Spine } from "pixi-spine";
+import { Sound } from "@pixi/sound";
 
 const app = new PIXI.Application();
 const mask = new PIXI.Graphics();
 const container = new PIXI.Container();
+const audioInstances = {};
 
-let baseAnimations;
+let anchor;
+let anchorAnimations;
+let anchorEvents;
+let symbolSpine;
 
 setup();
 
@@ -13,14 +18,16 @@ async function setup() {
   document.getElementById("app").appendChild(app.view);
   app.stage.interactive = true;
 
+  anchor = await loadAnchor();
+
   container.mask = mask;
   container.addChild(mask);
 
   mask.clear().beginFill(0x000000).drawRect(0, 0, 800, 310).endFill();
-
   mask.y = app.screen.height / 2 - mask.height / 2;
 
-  baseAnimations = await loadBaseAnimation();
+  anchorAnimations = anchor.spineData.animations;
+  anchorEvents = anchor.spineData.events;
 
   let reelIndex = 0;
   let reelPositionIndex = 0;
@@ -28,27 +35,63 @@ async function setup() {
   // Mock reels
   for (let i = 0; reelIndex < 5; i++) {
     for (let j = 0; reelPositionIndex < 10; j++) {
-      await PIXI.Assets.load("symbol/skeleton.json").then((res) => {
-        onAssetsLoaded(res, reelIndex, reelPositionIndex);
-      });
+      const randomSymbol = reelIndex === 2 && reelPositionIndex === 2;
+      const res = await PIXI.Assets.load(
+        !randomSymbol ? "symbol/skeleton.json" : "symbol2/skeleton.json"
+      );
+      onAssetsLoaded(res, reelIndex, reelPositionIndex);
       reelPositionIndex += 1;
     }
     reelIndex += 1;
     reelPositionIndex = 0;
   }
 
-  animateSpin();
+  await loadAudio("sounds/bonus_hit.mp3");
+
+  app.stage.on("pointerdown", () => {
+    animateSpin();
+  });
 
   app.stage.addChild(container);
 }
 
-function onAssetsLoaded(res, reelIndex, reelPositionIndex) {
-  let symbolSpine = new Spine(res.spineData);
+async function animateSpin() {
+  const allAnimations = ["animation_0", "animation_1", "animation_2"];
+  const delayBetweenAnimations = 500;
 
-  symbolSpine.spineData.animations = [
-    ...symbolSpine.spineData.animations,
-    ...baseAnimations,
-  ];
+  for (let j = 0; j < 5; j++) {
+    for (let i = 10 * j; i < 10 * (j + 1); i++) {
+      const animation = allAnimations[1];
+      const spine = container.children[i + 1];
+
+      setTimeout(() => {
+        spine.state.setAnimation(0, animation, false);
+      }, delayBetweenAnimations * j);
+    }
+  }
+}
+
+async function loadAnchor() {
+  return await PIXI.Assets.load("anchor/anchor.json").then((json) => {
+    return new Spine(json.spineData);
+  });
+}
+
+async function loadAudio(audioPath) {
+  const formatPath = audioPath.match("/([^/]+)$")[1];
+  if (!audioInstances[formatPath]) {
+    audioInstances[formatPath] = Sound.from({
+      url: audioPath,
+      preload: true,
+    });
+  }
+}
+
+async function onAssetsLoaded(res, reelIndex, reelPositionIndex) {
+  symbolSpine = new Spine(res.spineData);
+
+  symbolSpine.spineData.animations = [...anchorAnimations];
+  symbolSpine.spineData.events = [...anchorEvents];
 
   symbolSpine.x = app.screen.width / 10 + (reelIndex + 1) * 100;
   symbolSpine.y =
@@ -56,28 +99,31 @@ function onAssetsLoaded(res, reelIndex, reelPositionIndex) {
 
   symbolSpine.scale.set(0.475);
 
+  if (symbolSpine.spineData.slots[0].attachmentName === "symbol_1") {
+    registerSpineAudioEvent(symbolSpine, "bonus_hit");
+  }
+
   container.addChild(symbolSpine);
-  symbolSpine = undefined;
 }
 
-function animateSpin() {
-  const allAnimations = ["animation_0", "animation_1", "animation_2"];
+function playAudio(spine, eventName) {
+  const audioFileName = spine.spineData.events.find(
+    (event) => event.name === eventName
+  )?.audioPath;
 
-  let animation = allAnimations[0];
-
-  app.stage.on("pointerdown", async () => {
-    for (let j = 0; j < 5; j++) {
-      setTimeout(function () {
-        for (let i = 10 * j; i < 10 * (j + 1); i++) {
-          container.children[i + 1].state.setAnimation(0, animation, false);
-        }
-      }, 500 * j);
+  if (audioFileName) {
+    if (audioInstances[audioFileName]) {
+      audioInstances[audioFileName].play();
     }
-  });
+  }
 }
 
-async function loadBaseAnimation() {
-  return await PIXI.Assets.load("anchor/anchor.json").then((json) => {
-    return new Spine(json.spineData).spineData.animations;
+function registerSpineAudioEvent(spine, eventName) {
+  spine.state.addListener({
+    event: (trackIndex, event) => {
+      if (event.data.name === eventName) {
+        playAudio(spine, event.data.name);
+      }
+    },
   });
 }
